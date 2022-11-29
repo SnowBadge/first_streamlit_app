5274b959677599255f6ffe1c0e02589825ac5cc7
Prathmesh <119155093+SnowBadge@users.noreply.github.com>
2022-11-29 15:00:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,3 +34,10 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Take
 streamlit.dataframe(fruityvice_normalized) # output it to the screen as table using dataframes 
 
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
