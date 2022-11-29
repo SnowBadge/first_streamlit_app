021dc513ceb3ff2cb42804a1792f2876fe5cbf21
Prathmesh <119155093+SnowBadge@users.noreply.github.com>
2022-11-29 15:14:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Take
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized) # output it to the screen as table using dataframes 
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
