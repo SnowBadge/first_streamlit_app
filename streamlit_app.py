6a4d6f8416fe1720eefcd70541bd4f2d4be079c0
Prathmesh <119155093+SnowBadge@users.noreply.github.com>
2022-11-29 15:38:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,18 +24,17 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Take the json version of the response and normalize it
+    streamlit.dataframe(fruityvice_normalized) # output it to the screen as table using dataframes 
+except URLError as e:
+  streamlit.error()
 streamlit.write('The user entered ', fruit_choice)
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json()) # Just writes the data to the screen 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json()) # Take the json version of the response and normalize it
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized) # output it to the screen as table using dataframes 
-
 streamlit.stop()
 
 fruit_add = streamlit.text_input('What fruit would you like to add?','jackfruit')
